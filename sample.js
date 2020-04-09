27988c45fab9c4438769fa0cee285f2f7279df60
Trey Richbourg <trey@allprosystems.com>
2020-04-09 16:07:29 -0500

    Added sample.json

---
+++ sample.js
@@ -0,0 +1,23 @@
+myObj = [
+    {
+        "Id": "1",
+        "FirstName": "Trey",
+        "LastName": "Richbourg",
+        "Email": "treyrichbourg@gmail.com"
+    },
+    {
+        "Id": "2",
+        "FirstName": "Jane",
+        "LastName": "Doe",
+        "Email": "jane.doe@cbtnuggets.com"
+    },
+    {
+        "Id": "3",
+        "FirstName": "Pat",
+        "LastName": "Smith",
+        "Email": "pat.smith@cbtnuggets.com"
+    }
+]
+
+
+console.log(myObj[0]);
