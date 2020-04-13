7cf861ea67eba08439ce32475c4ad381502335ca
Trey Richbourg <trey@allprosystems.com>
2020-04-13 10:23:50 -0500

    Sample js/json updated

--- sample.js
+++ sample.js
@@ -1,23 +1,29 @@
 myObj = [
     {
-        "Id": "1",
+        "Id": 1,
         "FirstName": "Trey",
         "LastName": "Richbourg",
-        "Email": "treyrichbourg@gmail.com"
+        "Email": "treyrichbourg@gmail.com",
+        "Active" : true
     },
     {
-        "Id": "2",
+        "Id": 2,
         "FirstName": "Jane",
         "LastName": "Doe",
-        "Email": "jane.doe@cbtnuggets.com"
+        "Email": "jane.doe@cbtnuggets.com",
+        "Active" : false
     },
     {
-        "Id": "3",
+        "Id": 3,
         "FirstName": "Pat",
         "LastName": "Smith",
-        "Email": "pat.smith@cbtnuggets.com"
+        "Email": "pat.smith@cbtnuggets.com",
+        "Active" : true
     }
 ]
 
 
-console.log(myObj[0]);
+
+
+
+console.log(myObj[1].FirstName);
